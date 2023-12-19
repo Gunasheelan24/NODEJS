11cc71f12eee6718f681f35c4e13232ad2bfcf02
GunaSheelan A <140253087+Gunasheelan24@users.noreply.github.com>
2023-12-20 00:58:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const dotEnv = require("dotenv").config({ path: "./config.env" });
+const port = process.env.NODE_PORT || 3000;
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { appRouter } = require("./model/user");
+const { upload } = require("./middleware/File");
+require("./Database/connect");
+
+//MiddleWare's
+app.use(express.json());
+app.use(express.static("./dist"));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://parisairwaysversion.onrender.com",
+    methods: ["GET", "POST"],
+  })
+);
+app.options(
+  "https://parisairwaysversion.onrender.com",
+  cors({ preflightContinue: true })
+);
+app.use(upload.single("photo"));
+app.use(express.static("./png"));
+app.use(cookieParser());
+
+// Router
+app.use("/", appRouter);
+
+//Payment MiddleWare For Stripe
+
+//Port Listening
+
+app.listen(port, () => console.log("(❁´◡`❁)"));
